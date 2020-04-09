c333ccd69fdb917480ff96732155472a72f1a663
Costascons <42782142+Costascons@users.noreply.github.com>
2020-04-08 21:46:02 -0400

    More Light adjustments

--- Assets/Scripts/EffectVisuals/DayTransition.cs
+++ Assets/Scripts/EffectVisuals/DayTransition.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DayTransition : MonoBehaviour
 {
-    float speed = 1f;
+    float speed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
