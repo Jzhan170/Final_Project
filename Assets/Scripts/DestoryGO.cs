2ee12b4b40d126f086fd46c74f6dd3c121f71068
KorvusZ017 <42786143+KorvusZ017@users.noreply.github.com>
2020-04-19 19:43:58 -0400

    click indicator complete in test scene

---
+++ Assets/Scripts/DestoryGO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryGO : MonoBehaviour
+{
+    public float lifetime = .2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    
+}
