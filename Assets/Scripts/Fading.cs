aed1f48473084e19d3b871f1df1c5d1f9f9cd23c
KorvusZ017 <42786143+KorvusZ017@users.noreply.github.com>
2020-04-26 23:07:24 -0400

    Transition between fade out animation and ending scene complete, next working on the trigger of fade out

---
+++ Assets/Scripts/Fading.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fading : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Fade(1);
+        }
+    }
+
+    public void Fade(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadingShader");
+    }
+
+    public void FadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
